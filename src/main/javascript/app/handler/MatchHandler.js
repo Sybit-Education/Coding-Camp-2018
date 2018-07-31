8cbec2fe351388697e41191c0dac604afd333efa
enni2002 <enni2002@gmx.de>
2018-07-31 11:15:12 +0200

    Funktion der randoom linkerstellung. Fenster um den Link zu kopieren.

--- src/main/javascript/app/handler/MatchHandler.js
+++ src/main/javascript/app/handler/MatchHandler.js
@@ -1,8 +1,14 @@
 module.exports = {
     connectToMatch: function () {
-        let xhttp = new XMLHttpRequest();
         //TODO Abfrage um ein neues spiel zu generieren
-
+        let newMatch = new XMLHttpRequest();
+        newMatch.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200){
+                window.location.href = "/match/"+this.responseText;
+            }
+        }
+        newMatch.open("GET", "match/newmatch", true);
+        newMatch.send();
     },
 
     saveGamefield: function(matchId, message) {
